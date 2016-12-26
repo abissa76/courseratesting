474a5ff548a6b00018e22eab83484b00e5114a88
abissa76 <arvind@courseratesting.com>
2016-12-26 23:53:17 +0530

    my first assignment

---
+++ app.js
@@ -0,0 +1,29 @@
+(function(){
+'use strict';
+
+  angular.module('LunchCheck',[])
+
+    .controller('LunchCheckController',function($scope){
+
+        $scope.showMessage = function(name){
+              var stringtosplit = name;
+               var arrayofstring = stringtosplit.split(",");
+               var arraycount = arrayofstring.length;
+               if(arrayofstring == '')
+               arraycount = 0;
+
+
+              if(!arraycount){
+                $scope.Message = "please enter the data first";
+              }
+              else if(arraycount <= 3) {
+                 $scope.Message = "Enjoy";
+               }
+               else {
+                 $scope.Message = "Too Much";
+               }
+        return $scope.Message;
+          };
+    });
+
+})();
